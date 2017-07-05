be1cd33d8e1d7abda52c4630d68b9decd11246a3
Abhishar Jangir <abhishar.jangir@abspl.in>
2017-07-05 18:10:59 +0530

    amazon scrape

--- api/constants/constants.js
+++ api/constants/constants.js
@@ -1,3 +1,3 @@
-const WebUrl = 'https://www.flipkart.com/search?q=mobiles&otracker=start&as-show=on&as=off';
+const WebUrl = 'http://www.amazon.in/b/ref=s9_acss_bw_cg_Exclfarm_2f1_w?node=4363159031&pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-15&pf_rd_r=GX42BXZXTZ9QJ2NWHDPG&pf_rd_t=101&pf_rd_p=65ac4a8c-628d-44c9-bc49-56b33ac49c2d&pf_rd_i=1389401031';
 
 module.exports =  WebUrl ;
--- api/daos/scrapeDao.js
+++ api/daos/scrapeDao.js
@@ -1,33 +1,124 @@
 const Request = require('request-promise');
 const cheerio = require('cheerio');
-
-
+const Items = [];
 
 
 const ScrapeDao = (webURL) => {
-  return Request(webURL)
-  .then(data => processHtml(data))
-  .then(data => {return data})
-  .catch(err => {return err})
-}
-
-const processHtml = (html) => {
   return new Promise(function(resolve, reject) {
-    var mobile = {};
-    var $ = cheerio.load(html);
-    // $('._2-gKeQ').filter(function(){
-    //   var data = $(this);
-    //   var img = data.children('img');
-    //   console.log(img);
-    // })
-    // console.log($('._2-gKeQ').find('img')[0].attr('src'));
-    // $('._2-gKeQ').each(function(i, elem) {
-    //   console.log(elem);
-    //   mobile.images[i] = elem
-    //   // fruits[i] = $(this).text();
-    // });
-    resolve(mobile)
+    scraper(webURL);
   });
 }
+
+const scraper = (url) => {
+  Request(url).
+  then(x =>  {console.log(url);HTMLProcessor(x)})
+  .then(x => {
+    if (x.list.length > 0) {
+      Items.concat(x.list)
+    }
+    if (x.nextPage) {
+      scraper(x.nextPage);
+    }else {
+      return Items;
+    }
+  })
+}
+
+const HTMLProcessor = (html) => {
+  new Promise(function(resolve, reject) {
+    var $ = cheerio.load(html);
+    var itemLi = $(".s-result-item");
+    var mobileList = [];
+    $(itemLi).each(function(i, item) {
+      var name,image,price;
+      // Name
+      name = $(this).find('h2').text();
+      // Image
+      image = $(this).find('img').attr('src');
+      // price
+      if ($(this).find('.s-price').text() == '') {
+          price = $(this).find('.a-color-price').text()
+      } else {
+          price = $(this).find('.s-price').text()
+      }
+      mobileList.push({
+          name: name,
+          image: image,
+          price: price.replace(/\s/g, '')
+      });
+    });
+    var nextPageLink = $('#pagnNextLink').attr('href');
+    if (nextPageLink) {
+      nextPageLink = 'http://www.amazon.in/' + nextPageLink;
+       resolve({list : mobileList, nextPage : nextPageLink});
+    }else {
+      resolve({list : mobileList, nextPage : undefined});
+    }
+  });
+}
+//
+// const NextPageTracker = (html) => {
+//   let nextPageLink = $('#pagnNextLink').attr('href');
+//   if (nextPageLink) {
+//     nextPageLink = 'http://www.amazon.in/' + nextPageLink;
+//     // Do Somthing
+//      scraper(nextPageLink);
+//   }else {
+//     // Do Somthing
+//     scraper();
+//   }
+// }
+
+
+
+
+
+// const processHtml = (html) => {
+//   return new Promise(function(resolve, reject) {
+//     var mobile = [];
+//     var $ = cheerio.load(html);
+//     var imageList = $(".s-result-item");
+//     var productNameList = $('.s-color-twister-title-link');
+//     $(imageList).each(function(i, image){
+//       var name,image,price;
+//       // Name
+//       name = $(this).find('h2').text();
+//       // Image
+//       image = $(this).find('img').attr('src');
+//       // price
+//       if ($(this).find('.s-price').text() == '') {
+//         price = $(this).find('.a-color-price').text()
+//       }else {
+//         price = $(this).find('.s-price').text()
+//       }
+//       mobile.push({name : name,image : image,price :price.replace(/\s/g, '') });
+//     });
+//
+//     // var nextPage = 'http://www.amazon.in/' +  $('#pagnNextLink').attr('href');
+//     // if ($('#pagnNextLink').attr('href')) {
+//     //   console.log(nextPage);
+//     //   console.log(mobile);
+//     //   ScrapeDao(nextPage).then(e => resolve(e));
+//     // }else {
+//     //   console.log('Done');
+//     //   resolve(mobile);
+//     // }
+//     resolve(mobile);
+//
+//     // $('._3BTv9X').find('img');
+//     // (function(){
+//     //   var data = $(this);
+//     //   console.log(data.toArray());
+//     // })
+//     // console.log($('._2-gKeQ').find('img')[0].attr('src'));
+//     // $('._2-gKeQ').each(function(i, elem) {
+//     //   console.log(elem);
+//     //   mobile.images[i] = elem
+//     //   // fruits[i] = $(this).text();
+//     // });
+//
+//
+//   });
+
 
 module.exports = ScrapeDao;
