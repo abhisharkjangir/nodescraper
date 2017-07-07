afc1d913564f5a5f4cf0a4c348e05137ce80e7ce
Abhishar Jangir <abhisharjangir@ymail.com>
2017-07-07 18:42:09 +0530

    DBURL REMOVED

--- api/constants/database.js
+++ api/constants/database.js
@@ -1,5 +1,3 @@
-const  DBURL = 'mongodb://akayjangir:jangirakay@ds151082.mlab.com:51082/products';//Enter Your Database Url Here
-//var  DatabaseUrl = 'mongodb://username:dbpassword@ds053090.mlab.com:53090/omdb';
-// mongodb://<akayjangir>:<jangirakay>@ds151082.mlab.com:51082/products
+const  DBURL = 'mongodb://<username>:<password>@<mlab id>.mlab.com:51082/products';//Enter Your Database Url Here
 
 module.exports = DBURL;
